a8b1d779e399a8f20e81a13c4fe176902521278e
Humberto Corrêa <humberto.cgomes17@gmail.com>
2024-05-11 19:35:39 -0300

    flask and axios settings

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,29 @@
 import GoogleMapReact from "src/components/GoogleMap.js";
+import axios from "axios";
+import './globals.css';
 
 export default function Home() {
+
+   // new line start
+   function getData() {
+     axios({
+       method: "GET",
+       url:"/profile",
+     })
+     .then((response) => {
+       const res =response.data
+       setProfileData(({
+         profile_name: res.name,
+         about_me: res.about}))
+     }).catch((error) => {
+       if (error.response) {
+         console.log(error.response)
+         console.log(error.response.status)
+         console.log(error.response.headers)
+         }
+     })}
+  //end of new line 
+  
   return (
     <div>
       <h1>Teste</h1>
